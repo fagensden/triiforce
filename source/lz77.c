91cf6ff0c2a4f52b2e440f3750a397bbd6d230d9
nintendo4ever <nintendo4ever@kaffeeschluerfer.com>
2009-08-05 12:45:29 +0000

    * Updated copyright statements

--- source/lz77.c
+++ source/lz77.c
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * lz77.c
+ *
+ * Copyright (c) 2009 The Lemon Man
+ * Copyright (c) 2009 Nicksasa
+ * Copyright (c) 2009 WiiPower
+ *
+ * Distributed under the terms of the GNU General Public License (v2)
+ * See http://www.gnu.org/licenses/gpl-2.0.txt for more info.
+ *
+ * Description:
+ * -----------
+ *
+ ******************************************************************************/
+
 #include <gccore.h>
 #include <malloc.h>
 #include <stdio.h>
--- source/tools.c
+++ source/tools.c
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * tools.c
+ *
+ * Copyright (c) 2009 The Lemon Man
+ * Copyright (c) 2009 Nicksasa
+ * Copyright (c) 2009 WiiPower
+ *
+ * Distributed under the terms of the GNU General Public License (v2)
+ * See http://www.gnu.org/licenses/gpl-2.0.txt for more info.
+ *
+ * Description:
+ * -----------
+ *
+ ******************************************************************************/
+
 #include <gccore.h>
 #include <malloc.h>
 #include <stdio.h>
@@ -8,7 +23,7 @@
 #include "tools.h"
 
 #define TRIIFORCE_MAJOR 0
-#define TRIIFORCE_MINOR 2
+#define TRIIFORCE_MINOR 3
 
 void printheadline()
 {
