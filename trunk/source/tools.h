3fd305f5014ce1b9e4cb03e79ccf1f2219217fd4
nintendo4ever@kaffeeschluerfer.com <nintendo4ever@kaffeeschluerfer.com@1380b982-7df0-11de-97b4-09f1478e008f>
2009-08-17 19:07:18 +0000

    * Added power button handling * Minor changes * Dirty bug fix for ISFS_ReadDir

--- trunk/source/tools.h
+++ trunk/source/tools.h
@@ -17,6 +17,9 @@
 #define TITLE_LOWER(x)		((u32)(x))
 #define TITLE_ID(x,y)		(((u64)(x) << 32) | (y))
 
+bool Power_Flag;
+bool Reset_Flag;
+
 void *allocate_memory(u32 size);
 s32 read_file(char *filepath, u8 **buffer, u32 *filesize);
 s32 identify(u64 titleid, u32 *ios);
